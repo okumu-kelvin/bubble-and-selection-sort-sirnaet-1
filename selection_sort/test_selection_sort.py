db9bde7bbc1c528e8c63c3555762c3f8557b6849
Brian S. Memusi <bsmemusi@gmail.com>
2025-06-20 12:16:07 +0300

    Update test_selection_sort.py

--- selection_sort/test_selection_sort.py
+++ selection_sort/test_selection_sort.py
@@ -1,10 +1,10 @@
-from selection_sort2 import selection_sort2
+from selection_sort import selection_sort
 
 def test_sorted():
-    assert selection_sort2([1, 2, 3]) == [1, 2, 3]
+    assert selection_sort([1, 2, 3]) == [1, 2, 3]
 
 def test_reverse():
-    assert selection_sort2([3, 2, 1]) == [1, 2, 3]
+    assert selection_sort([3, 2, 1]) == [1, 2, 3]
 
 def test_duplicates():
-    assert selection_sort2([29, 10, 14, 37, 14]) == [10, 14, 14, 29, 37]
+    assert selection_sort([29, 10, 14, 37, 14]) == [10, 14, 14, 29, 37]
