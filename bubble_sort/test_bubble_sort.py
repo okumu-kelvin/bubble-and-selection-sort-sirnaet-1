98bc652d5252a008e31a52a7a02e86713aaa8bf0
Brian S. Memusi <bsmemusi@gmail.com>
2025-06-20 12:02:55 +0300

    Update test_bubble_sort.py

--- bubble_sort/test_bubble_sort.py
+++ bubble_sort/test_bubble_sort.py
@@ -1,10 +1,10 @@
 from bubble_sort2 import bubble_sort2
 
 def test_sorted():
-    assert bubble_sort([1, 2, 3]) == [1, 2, 3]
+    assert bubble_sort2([1, 2, 3]) == [1, 2, 3]
 
 def test_reverse():
-    assert bubble_sort([3, 2, 1]) == [1, 2, 3]
+    assert bubble_sort2([3, 2, 1]) == [1, 2, 3]
 
 def test_duplicates():
-    assert bubble_sort([4, 5, 3, 4]) == [3, 4, 4, 5]
+    assert bubble_sort2([4, 5, 3, 4]) == [3, 4, 4, 5]
