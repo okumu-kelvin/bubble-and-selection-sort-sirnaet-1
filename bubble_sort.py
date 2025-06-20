58c6b00fecc92b6ba2ec09e3ae4d62ba39ca920c
Brian S. Memusi <bsmemusi@gmail.com>
2025-06-20 11:54:12 +0300

    Add files via upload

---
+++ bubble_sort.py
@@ -0,0 +1,7 @@
+def bubble_sort(unsorted_list):
+    n = len(unsorted_list)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+    return unsorted_list
